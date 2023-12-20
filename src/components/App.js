9ac296dbec11a37baed8fc3448d9f4475683bf72
VamshiRk27 <vamshi.rk27@gmail.com>
2023-12-20 23:34:52 +0530

    First Try

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [name,setName] = useState("");
+
+  function show(){
+    name=setName(document.getElementById("user").value);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      <label>Enter your name:</label> <br/>
+        <input type="text" onChange={show} id="user"/>
+        {
+          name!="" && <p>Hello {name}!</p>
+        }
     </div>
   )
 }
